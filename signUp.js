68b25ef6674dc086b45027340b517a7335826e7f
Rajnish <rajnishkumar200216@gmail.com>
2026-01-16 16:57:47 +0530

    StudentManagementSystem Project complete

---
+++ signUp.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const signupBtn = document.querySelector("button");
+
+  signupBtn.addEventListener("click", () => {
+    const fname = document.getElementById("fname").value;
+    const lname = document.getElementById("lname").value;
+    const email = document.getElementById("Email").value;
+    const password = document.getElementById("Password").value;
+    const age = document.getElementById("Age").value;
+
+    
+
+    if (fname === "" || lname === "" || email==="" || password==="" || age === "") {
+      alert("All fields are required");
+      return;
+    }
+
+    if (email.endsWith("@gmail.com")) {
+    } else {
+      alert("Only Gmail allowed");
+      return;
+    }
+    if(password.length >=6){
+
+    }else{
+        alert("Password must be at least 6 characters");
+        return;
+    }
+
+    localStorage.setItem("fname", fname);
+    localStorage.setItem("lname", lname);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
+    localStorage.setItem("age", age);
+
+    alert("Signup Successful!");
+    signupBtn.style.AnimationOfButton;
+    window.location.href = "/login.html";
+  });
+});
